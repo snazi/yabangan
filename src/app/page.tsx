c9bf18fd98c87fba2ecaf742921383821af88aef
Angelo Amadora <amadora.angelo@gmail.com>
2024-06-13 16:49:50 +0800

    updated to use a simple json file as data source

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
 // app/page.tsx
+import { promises as fs } from 'fs';
+
 type Season = {
   id: number;
   name: string;
 };
 
-const seasons: Season[] = [
-  { id: 1, name: 'Season 1' },
-  { id: 2, name: 'Season 2' },
-  { id: 3, name: 'Season 3' },
-];
+export default async function Home() {
 
-export default function Home() {
+  const file = await fs.readFile(process.cwd() + '/public/data.json', 'utf8');
+  console.log("test")
+  const seasons = JSON.parse(file);
+
+  console.log("something"+ seasons)
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
       <h1 className="text-4xl font-semibold mb-8 text-gray-800">Yabangan League Seasons</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-4">
-        {seasons.map((season) => (
+        {seasons['seasons'].map((season) => (
           <a
             key={season.id}
             href={`/season/${season.id}`}
